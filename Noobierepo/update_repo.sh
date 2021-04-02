7d3685b4024b4bc1617dec79779db412f0d77420
PeterDauwe <bobo5290461@gmail.com>
2021-04-02 05:12:36 +0200

    up

--- Noobierepo/update_repo.sh
+++ Noobierepo/update_repo.sh
@@ -5,32 +5,11 @@ rm noobie-repo*
 
 echo "repo-add"
 #repo-add -s -n -R arcolinux_repo.db.tar.gz *.pkg.tar.xz
-repo-add -s -n -R noobie-repo.db.tar.gz *.pkg.tar.zst
+repo-add noobie-repo.db.tar.gz *.pkg.tar.zst
 #sleep 8
-cp -f noobie-repo.db.tar.gz noobie_repo.db
+#cp -f noobie-repo.db.tar.gz noobie_repo.db
 
 echo "####################################"
 echo "Repo Updated!!"
 echo "####################################"
 
-
-#!/bin/bash
-
-
-#echo "Clean out old db"
-#echo
-
-#rm noobie-repo*
-
-#echo
-#echo "Run repo-add"
-#echo 
-
-#repo-add noobie-repo.db.tar.gz *.pkg.tar.zst
-
-##sleep 8
-#cp -f noobie-repo.db.tar.gz noobie-repo.db
-
-#echo "####################################"
-#echo "Repo Updated!!"
-#echo "####################################"
